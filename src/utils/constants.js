8f52d6ec0dc849839f88e33a501a57ec2676c5e1
Hannah Nordgren <hey@hereshannahs.info>
2020-02-03 23:41:37 -0500

    [FEATURE] set up basic sizes and colors in constants

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const colors = {
+	black: '#141414',
+	white: '#ffffff',
+	borderGray: '#e7e7e7',
+	green: '#34df6b',
+	red: '#d42828',
+	ltNavy: '#2a3151',
+	dkNavy: '#0c122f',  
+};
+
+export const sizes = {
+	xsmall: '5px',
+	small: '10px',
+	medium: '20px',
+	large: '40px',
+	xlarge: '80px',
+};
