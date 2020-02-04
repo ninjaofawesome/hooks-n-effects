8467c8357f634a11fd68645938f62eb182905f08
Hannah Nordgren <hey@hereshannahs.info>
2020-02-03 23:42:21 -0500

    [MISC] forgot to save

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,20 @@
+export const sizes = {
+	xsmall: '5px',
+	small: '10px',
+	medium: '20px',
+	large: '40px',
+	xlarge: '80px',
+};
+
+// roughly modeled around the iphone sizes, but more could be added here.
+export const breakpoints = {
+	mobile: '375px';
+	mobileLandscape: '768px',
+	tablet: '768px',
+	tabletLandscape: '1024px',
+	desktop: '1025px'
+};
+
 export const colors = {
 	black: '#141414',
 	white: '#ffffff',
@@ -6,12 +23,4 @@ export const colors = {
 	red: '#d42828',
 	ltNavy: '#2a3151',
 	dkNavy: '#0c122f',  
-};
-
-export const sizes = {
-	xsmall: '5px',
-	small: '10px',
-	medium: '20px',
-	large: '40px',
-	xlarge: '80px',
 };
