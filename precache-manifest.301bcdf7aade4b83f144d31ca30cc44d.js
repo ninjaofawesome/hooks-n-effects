65f40cee76aa0a93e5a5055cb0928c3d9e6f7e28
Hannah Nordgren <hey@hereshannahs.info>
2020-03-10 00:37:35 -0400

    Updates

---
+++ precache-manifest.301bcdf7aade4b83f144d31ca30cc44d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f316ae3ab535d1436eb539b63a88db7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45a191a9b4d9adbdada6",
+    "url": "/static/js/2.a329e6e2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a329e6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5390bc717f7bd8ebbe",
+    "url": "/static/js/main.161b074a.chunk.js"
+  },
+  {
+    "revision": "564555dbbd61d4846201",
+    "url": "/static/js/runtime-main.d40988dd.js"
+  }
+]);
